f0e901542a65c24813e1ee115c877178a79767d7
Maurice Schoenmakers <m@x-x.io>
2014-03-31 00:38:47 +0200

    fixed script

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 $DIR/../node_modules/mocha/bin/mocha --ui tdd $1/*.tdd.test.js
 if [[ -n "$TRAVIS" ]] ; then
-	node $DIR/..node_modules/zuul/bin/zuul --ui mocha-tdd -- test/common.tdd.test.js
+	node $DIR/../node_modules/zuul/bin/zuul --ui mocha-tdd -- test/common.tdd.test.js
 fi
