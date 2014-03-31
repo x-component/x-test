047d90e6222635a5e5f891535dda9c348f9c05df
Maurice Schoenmakers <m@x-x.io>
2014-03-31 10:47:40 +0200

    removed zuul call

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 $DIR/../node_modules/mocha/bin/mocha --ui tdd $1/*.tdd.test.js
-if [[ -n "$TRAVIS" ]] ; then
-	node $DIR/../node_modules/zuul/bin/zuul --ui mocha-tdd -- test/*.tdd.test.js
-fi
+#if [[ -n "$TRAVIS" ]] ; then
+#	node $DIR/../node_modules/zuul/bin/zuul --ui mocha-tdd -- test/*.tdd.test.js
+#fi
